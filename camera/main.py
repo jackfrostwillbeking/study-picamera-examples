4c9a17b152832358e3291fef7579310d28a8ddfd
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-04-23 12:00:02 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -10,6 +10,9 @@ from pyzbar import pyzbar
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from datetime import datetime
+import numpy as np
+import cv2
+import time
 
 camera = PiCamera()
 camera.resolution = (640, 480)
