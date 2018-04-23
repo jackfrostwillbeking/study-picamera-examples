f6b603bad0cd6cdab46961548de3980a8818206c
kawasakiya <31604636+jackfrostwillbeking@users.noreply.github.com>
2018-04-23 19:56:25 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -9,24 +9,8 @@ import time
 import threading
 import os
 
-from pyzbar import pyzbar
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from datetime import datetime
-import numpy as np
-import cv2
-import time
-
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-time.sleep(0.1)
-
 app = Flask(__name__)
-
-# video_camera = VideoCamera(flip=False)
-
+video_camera = VideoCamera(flip=False)
 
 @app.route('/')
 def index():
@@ -35,51 +19,18 @@ def index():
     return render_template('index.html', message=message)
 
 
-# def gen(camera):
-def gen():
+def gen(camera):
     while True:
-#         frame = camera.get_frame()
-        frame = get_frame()
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-def get_frame():
-    camera.capture(rawCapture, format="bgr", use_video_port=True)
-    frame = rawCapture.array
-    decoded_objs = decode(frame)
-    frame = display(frame, decoded_objs)
-    ret, jpeg = cv2.imencode('.jpg', frame)
-    rawCapture.truncate(0)
-
-    return jpeg.tobytes()
-
-def decode(frame):
-    decoded_objs = pyzbar.decode(frame, scan_locations=True)
-    for obj in decoded_objs:
-        print(datetime.now().strftime('%H:%M:%S.%f'))
-        print('Type: ', obj.type)
-        print('Data: ', obj.data)
-        
-    return decoded_objs
-
-def display(frame, decoded_objs):
-    for obj in decoded_objs:
-        left, top, width, height = obj.rect
-        frame = cv2.rectangle(frame,
-                              (left, top),
-                              (left + width, height + top),
-                              (0, 0, 255), 2)
-        data = obj.data.decode('utf-8')
-        cv2.putText(frame,data,(left,top),cv2.FONT_HERSHEY_PLAIN, 2,(0, 0, 255))
-
-    return frame
         
 @app.route('/video_feed')
 def video_feed():
-#     return Response(gen(video_camera),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-    return Response(gen(),
+    return Response(gen(video_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, threaded=True)
